f7957be9fbad375739ffe242db6bb509fc08f152
elena <lin.elena@franchettisalviani.net>
2025-03-14 17:12:35 +0100

    ver1.1

--- impiccato/obj/Debug/net8.0/impiccato.AssemblyInfo.cs
+++ impiccato/obj/Debug/net8.0/impiccato.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("impiccato")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f1e1cca59f35fb721f31b88dc10387c77c51df1")]
 [assembly: System.Reflection.AssemblyProductAttribute("impiccato")]
 [assembly: System.Reflection.AssemblyTitleAttribute("impiccato")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
